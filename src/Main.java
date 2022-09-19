344ee72456bcc801195ec662ef72d90ac89d9af0
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-09-19 12:38:13 +0300

    change files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amount = 12_499; // стоимость билета
+        int percent = 20; // кол-во рублей для одной бонусной милли
+        int bonus = amount / percent; // Количество бонусных милль
+        System.out.println("Roman, hello! Thank you for choosing our airline for your flight. Your bonus is: "+ bonus +" miles");
     }
 }
